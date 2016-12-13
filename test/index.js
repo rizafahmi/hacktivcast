d66ac61ddae9dcda7e7caa59cbad4d2f580f21d9
Riza Fahmi <rizafahmi@gmail.com>
2016-12-14 06:29:57 +0700

    Add our first test. Testing all shows

---
+++ test/index.js
@@ -0,0 +1,27 @@
+process.env.NODE_ENV = 'test'
+
+const chai = require('chai')
+const should = chai.should()
+const chaiHttp = require('chai-http')
+const server = require('../app.js')
+
+chai.use(chaiHttp)
+
+describe('API routes', () => {
+  it('should return all shows', (done) => {
+    chai.request(server)
+      .get('/api/v1/shows')
+      .end((err, res) => {
+        res.should.have.status(200)
+        res.should.be.json
+        res.body.should.be.a('array')
+        res.body.length.should.equal(2)
+        res.body[0].should.have.property('title')
+        res.body[0].title.should.equal('HACKTIVcast Series')
+        res.body[0].should.have.property('description')
+        res.body[0].description.should.equal('HACTIVcast Labs')
+
+        done()
+      })
+  })
+})
