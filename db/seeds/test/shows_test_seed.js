032c921e6da6e8caa6d85c5bdda213a62649c10d
Riza Fahmi <rizafahmi@gmail.com>
2016-12-13 22:18:22 +0700

    Seeding the db

---
+++ db/seeds/test/shows_test_seed.js
@@ -0,0 +1,11 @@
+exports.seed = function (knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('shows').del()
+    .then(function () {
+      return Promise.all([
+        // Inserts seed entries
+        knex('shows').insert({title: 'HACTIVcast Series', description: 'Sebuah screencast series tentang pemrograman NodeJS, JavaScript, GIT, GitHub, VIM, editor dan teknologi lain yang terkait'}),
+        knex('shows').insert({title: 'HACKTIVcast Labs', description: 'Sebuah unscripted live coding session dengan para expert, technology enthusiast dan coder professional'})
+      ])
+    })
+}
