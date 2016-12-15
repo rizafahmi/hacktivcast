21f3050f16185a0a6fdb575393f2192751005ddc
Riza Fahmi <rizafahmi@gmail.com>
2016-12-15 10:03:12 +0700

    Finish getting data from the backend

--- src/components/Show.js
+++ src/components/Show.js
@@ -7,10 +7,10 @@ class Show extends React.Component {
       <Card rounded={true} width={418}>
         <CardImage src='http://placehold.it/512/08e/fff' />
         <Heading level={3} size={3} style={{marginBottom: 20}}>
-          HACKTIVcast Series
+          {this.props.title}
         </Heading>
         <Text style={{marginBottom: 20}}>
-          Sebuah screencast series tentang pemrograman NodeJS, JavaScript, GIT, GitHub, VIM, editor dan teknologi lain yang terkait.
+          {this.props.description}
         </Text>
         <Button
           href='#'
