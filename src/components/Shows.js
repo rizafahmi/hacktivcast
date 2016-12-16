9aa99bdbd111466432da06fd5228f57b4b17bc5c
Riza Fahmi <rizafahmi@gmail.com>
2016-12-16 11:47:46 +0700

    Finish showing from data

--- src/components/Shows.js
+++ src/components/Shows.js
@@ -25,9 +25,11 @@ class Shows extends React.Component {
       <article className='bt b--black-10 bg-washed-blue mid-gray ph3 ph5-ns pv4 pv5-ns'>
         <div className='mw9 center'>
           <h3 className='mt0 f5 f3-ns'>Shows</h3>
-      {this.state.shows.map(show) => {
-        return <Show />
-      }}
+          {this.state.shows.map(show => {
+             return (
+               <Show />
+             )
+           })}
         </div>
       </article>
     )
