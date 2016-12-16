48f327e036453da9f6091363507d0c59202143d9
Riza Fahmi <rizafahmi@gmail.com>
2016-12-16 23:14:05 +0700

    Setup our first react test

--- test/frontend/index.js
+++ test/frontend/index.js
@@ -4,15 +4,15 @@ import { mount, shallow } from 'enzyme'
 import jsdom from 'jsdom'
 
 const App = require('../../src/components/App.js')
+const Menu = require('../../src/components/Menu.js')
 
 const doc = jsdom.jsdom('<!doctype html><html><body></body></html>')
 global.document = doc
 global.window = doc.defaultView
 
-describe('<App />', () => {
-  it('renders', (done) => {
+describe('Testing App component', () => {
+  it('will render Menu', () => {
     const wrapper = mount(<App />)
-    expect(wrapper.find('small').text()).to.equal('by HACKTIV8')
-    done()
+    expect(wrapper.contains(<Menu />)).to.be.true
   })
 })
