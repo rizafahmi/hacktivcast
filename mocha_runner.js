89cf676cc4ba6cb86b5b0c06be7cf2a850af5dd3
Riza Fahmi <rizafahmi@gmail.com>
2016-12-17 06:07:55 +0700

    Finish setting up mocha and enzyme

--- mocha_runner.js
+++ mocha_runner.js
@@ -5,17 +5,16 @@ let jsdom = require('jsdom').jsdom
 let exposedProperties = ['window', 'navigator', 'document']
 
 global.document = jsdom('')
-global.window = document.defaultView;
+global.window = document.defaultView
 Object.keys(document.defaultView).forEach(property => {
   if (typeof global[property] === 'undefined') {
     exposedProperties.push(property)
     global[property] = document.defaultView[property]
   }
-}
+})
 
 global.navigator = {
   userAgent: 'node.js'
 }
 
 documentRef = document
-
